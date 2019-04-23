5c4e839db1851cff4e480474a600d255196c6ed9
concurrent-input-6175 <xywong23@gmail.com>
2019-04-23 07:59:20 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,14 +1,15 @@
-def display_board (board)
 
+board = [" "," "," "," "," "," "," "," "," "]
+
+def display_board (board)
    puts " #{board[0]} | #{board[1]} | #{board[2]} "
    puts "-----------"
    puts " #{board[3]} | #{board[4]} | #{board[5]} "
    puts "-----------"
    puts " #{board[6]} | #{board[7]} | #{board[8]} "
-
 end
 
-board = [" "," "," "," "," "," "," "," "," "]
+
 
 def valid_move?
   if index.between?(0,8) 
