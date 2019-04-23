37d9266468d51eb3e49ee0e441045bd4d4ab2b95
concurrent-input-6175 <xywong23@gmail.com>
2019-04-23 08:00:35 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -9,6 +9,11 @@ def display_board (board)
    puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+
 def valid_move?
   if index.between?(0,8) 
     if board[index] != "X" &&  board[index] != "O"
